c27e14a657135e394f8905ba28e8a960377923a1
miguel <miguel_silva159@estudante.sesisenai.org.br>
2025-09-24 16:30:37 -0300

    calculos.js

---
+++ prog08.js
@@ -0,0 +1,24 @@
+
+// um sistema de controle de temperatura esta sendo monitorada pelo arduino
+// a escala do sensor de temperatura vai de 0°C até 100°C
+// a escala no arduino vai de 0V até 5V
+//qual o valor que o programa deve mostrar se o sensor medir 37°C?
+
+
+function principal(){
+
+let tempMaxima = 100, tensaoArduino = 5, tempSensor = 37
+
+let resArduino = ( tensaoArduino * tempSensor ) /tempMaxima
+
+    console.log(`o valor da tensão no Arduino é: ${resArduino.toFixed(2)} V`)
+    console.log(`para uma temperatura de ${tempSensor} °C`)
+
+
+
+
+
+
+}
+
+principal()
